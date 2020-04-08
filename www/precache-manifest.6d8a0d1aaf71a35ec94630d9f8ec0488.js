bfb160c2e2bc925beeaf929c9b598f6c6c14d953
dedabyte <zwerrr@gmail.com>
2020-04-08 18:53:02 +0200

    build 1.0.0

---
+++ www/precache-manifest.6d8a0d1aaf71a35ec94630d9f8ec0488.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7249a9b818884ef4ced6763acae332",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01acebe2cda9e348b2c0",
+    "url": "./static/css/main.b0a13aec.chunk.css"
+  },
+  {
+    "revision": "0635ca5c61289aa1be11",
+    "url": "./static/js/2.74f2dd39.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.74f2dd39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01acebe2cda9e348b2c0",
+    "url": "./static/js/main.1ea2d45a.chunk.js"
+  },
+  {
+    "revision": "31aea1fc522ba5e2cba1",
+    "url": "./static/js/runtime-main.7270de16.js"
+  },
+  {
+    "revision": "90cf00b24e6ecefc69c86d0b4d2f5118",
+    "url": "./static/media/friends_logo_black.90cf00b2.png"
+  }
+]);

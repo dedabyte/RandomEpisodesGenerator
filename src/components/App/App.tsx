d0892f2dc31072da239f1193daddcfa4e604fece
dedabyte <zwerrr@gmail.com>
2020-04-10 19:40:05 +0200

    simplify App html layout, optimize animations

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import c from './App.module.scss';
 import cls from 'classnames';
 import { ProgressBar } from '../ProgressBar';
-import { makeSE } from '../../utils';
+import { makeSE, triggerAnimation } from '../../utils';
 import { GeneratedData } from '../../types';
 import { generateNextEpisode } from '../../utils/generator';
 import { Info } from '../Info';
@@ -22,17 +22,22 @@ export const App = () => {
 
 	const [showPlot, setShowPlot] = useState(true);
 
-	const handleGenerate = () => {
-		const generatedData = generateNextEpisode();
+	const titleWrapRef = useRef(null);
+	const progressWrapRef = useRef(null);
+	const episodeWrapRef = useRef(null);
 
+	const handleGenerate = () => {
 		if (init) {
 			setIsChanging(true);
 			setTimeout(() => setIsChanging(false), 700);
+		} else {
+			triggerAnimation(titleWrapRef.current, c.initTitleWrap);
+			triggerAnimation(progressWrapRef.current, c.initProgressWrap);
+			triggerAnimation(episodeWrapRef.current, c.initEpisodeWrap);
+			setInit(true);
 		}
 
-		setInit(true);
-
-		setGenerated(generatedData);
+		setGenerated(generateNextEpisode());
 	};
 
 	const handleTogglePlot = () => setShowPlot(!showPlot);
@@ -42,18 +47,18 @@ export const App = () => {
 
 			{init && <Info/>}
 
-			<div className={c.titleWrap}>
+			<div className={c.titleWrap} ref={titleWrapRef}>
 				<div className={c.title}/>
 				<div className={c.subtitle}>Random Episodes Generator</div>
 			</div>
 
-			<div className={c.progressWrap}>
+			<div className={c.progressWrap} ref={progressWrapRef}>
 				<ProgressBar title={'SEASON'} value={generated.seasonIndex + 1} progress={generated.seasonProgress}/>
 				<div className={c.progressDivider}/>
 				<ProgressBar title={'EPISODE'} value={generated.episodeIndex + 1} progress={generated.episodeProgress}/>
 			</div>
 
-			<div className={cls(c.episodeWrap, isChanging && c.changeEpisode)}>
+			<div className={cls(c.episodeWrap, isChanging && c.changeEpisode)} ref={episodeWrapRef}>
 				<div className={c.title}>{generated.episode?.title}</div>
 				<div className={c.cover}
 						 style={{ backgroundImage: `url(./img/${makeSE(generated.seasonIndex, generated.episodeIndex)}.png)` }}>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,12 @@ export const parseSE = (se: string) => {
 		eIdx: parseInt(split[1]),
 	}
 };
+
+export const triggerAnimation = (ref: any, className: string) => {
+	if (!ref) {
+		console.warn('triggerAnimation: ref is null');
+		return;
+	}
+	ref.classList.add(className);
+	ref.addEventListener('animationend', () => ref.classList.remove(className), { once: true });
+}
